77d1bdb00477b748a0c22e8ccc0f34cd1430949e
Umair-Iqbal <51910760+umairiqbal78@users.noreply.github.com>
2021-07-09 15:56:48 +0500

    updating ui changes

--- lib/cal.dart
+++ lib/cal.dart
@@ -7,89 +7,156 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
+  final ButtonStyle style = ElevatedButton.styleFrom(
+      primary: Colors.white,
+      minimumSize: Size(80, 60),
+      textStyle: const TextStyle(
+        fontSize: 30,
+      ),
+      shape: CircleBorder(
+        side: BorderSide(
+          color: Colors.transparent,
+        ),
+      ),
+      shadowColor: Colors.transparent);
+  var operStyle = TextStyle(color: Colors.green);
+  var numStyle = TextStyle(color: Colors.grey);
+  
   var result = "";
-  Widget btn(var textt) {
+  Widget btn(var textt, var btnstyle) {
     return ElevatedButton(
+      style: style,
       onPressed: () {
         setState(() {
           result = result + textt;
         });
       },
-      child: Text(textt),
+      child: Text(
+        textt,
+        style: btnstyle,
+      ),
     );
   }
 
   clearr() {
     setState(() {
+      expression = "";
       result = "";
     });
   }
 
+  dynamic expression = "";
   output() {
     Parser p = Parser();
+    expression = result;
     Expression exp = p.parse(result);
+
     ContextModel cm = ContextModel();
     double eval = exp.evaluate(EvaluationType.REAL, cm);
+    
 
     setState(() {
       result = eval.toString();
+      expression = expression;
+
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(result,
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            )),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            btn("1"),
-            btn("2"),
-            btn("3"),
-            btn("+"),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            btn("4"),
-            btn("5"),
-            btn("6"),
-            btn("-"),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            btn("7"),
-            btn("8"),
-            btn("9"),
-            btn("*"),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            ElevatedButton(onPressed: clearr, child: Text("clear")),
-            btn("0"),
-            ElevatedButton(onPressed: output, child: Text("=")),
-            btn("/"),
-          ],
-        ),
-      ],
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      margin: EdgeInsets.only(left: 10, top: 10, right: 10, bottom: 10),
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10),
+            topRight: Radius.circular(10),
+            bottomLeft: Radius.circular(10),
+            bottomRight: Radius.circular(10)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(expression,
+              style: TextStyle(
+                fontSize: 36,
+                color : Colors.grey,
+              )),
+          Text(result,
+              style: TextStyle(
+                fontSize: 56,
+                fontWeight: FontWeight.bold,
+              )),
+          SizedBox(
+            height: 100,
+          ),
+          Row(
+            children: [
+              btn("1", numStyle),
+              btn("2", numStyle),
+              btn("3", numStyle),
+              btn("/", operStyle)
+            ],
+          ),
+          SizedBox(
+            height: 15.0,
+          ),
+          Row(
+            children: [
+              btn("4", numStyle),
+              btn("5", numStyle),
+              btn("6", numStyle),
+              btn("*", operStyle),
+            ],
+          ),
+          SizedBox(
+            height: 15.0,
+          ),
+          Row(
+            children: [
+              btn("7", numStyle),
+              btn("8", numStyle),
+              btn("9", numStyle),
+              btn("+", operStyle),
+            ],
+          ),
+          SizedBox(
+            height: 15.0,
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                  style: style,
+                  onPressed: clearr,
+                  child: Text(
+                    "AC",
+                    style: TextStyle(color: Colors.grey),
+                  )),
+              btn("0", numStyle),
+              ElevatedButton(
+                  style: style,
+                  onPressed: output,
+                  child: Text(
+                    "=",
+                    style: TextStyle(color: Colors.green),
+                  )),
+              btn("-", operStyle),
+            ],
+          ),
+        ],
+      ),
     );
   }
-}
-
-Widget btn(var textt) {
-  return ElevatedButton(
-    onPressed: () {},
-    child: Text(textt),
-  );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'cal.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        body: Calculator(),
+      home: SafeArea(
+        child: Calculator(),
       ),
     ));
